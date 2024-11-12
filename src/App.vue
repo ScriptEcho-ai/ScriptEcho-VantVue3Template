59acb5d96f8ea245e6cf3b53d86b705c9baec61c
huangzhuoran <huangzhuoran@haomo-studio.com>
2024-11-12 17:54:30 +0800

    fix: 修改README

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <a-config-provider :locale="zhCN">
-    <router-view />
-  </a-config-provider>
+  <router-view />
 </template>
 
 <script>
-import zhCN from 'ant-design-vue/es/locale/zh_CN';
 export default {
   name: 'App',
   components: {
@@ -13,7 +10,6 @@ export default {
   },
   data(){
     return {
-      zhCN,
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import Antd from "ant-design-vue";
+import Vant from "vant";
+import 'vant/lib/index.css';
 import router from "./router/index.js";
 
 const app = createApp(App);
-app.use(Antd);
+app.use(Vant);
 app.use(router);
 app.mount("#app");
